00913d21f837ede2f1583acfed8fe3e0857ab2db
Naveen <naveenponnambalam2005@gmail.com>
2025-08-13 17:49:54 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCO-d_Xk_v0I-6QBXIrVjAKoe6ecbihkOI",
-  authDomain: "my-contact-app-e0f43.firebaseapp.com",
-  projectId: "my-contact-app-e0f43",
-  storageBucket: "my-contact-app-e0f43.firebasestorage.app",
-  messagingSenderId: "746835891227",
-  appId: "1:746835891227:web:269be19d3824282caed4be",
-  measurementId: "G-77P9XF1BJH"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -22,3 +14,4 @@ const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 
 export default app;
+
